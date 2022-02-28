93edd55b09d6eb9cf8e120a14cf975878731da84
Hans Zhang <>
2022-02-27 19:09:24 -0800

    task1-4

--- src/student_code.cpp
+++ src/student_code.cpp
@@ -14,9 +14,14 @@ namespace CGL
    * @return A vector containing intermediate points or the final interpolated vector
    */
   std::vector<Vector2D> BezierCurve::evaluateStep(std::vector<Vector2D> const &points)
-  { 
+  {
     // TODO Part 1.
-    return std::vector<Vector2D>();
+    std::vector<Vector2D> output;
+    for (int i = 0; i < points.size() - 1; ++i) {
+      output.push_back((1 - this->t) * points[i] + this->t * points[i+1]);
+
+    }
+    return output;
   }
 
   /**
@@ -30,7 +35,12 @@ namespace CGL
   std::vector<Vector3D> BezierPatch::evaluateStep(std::vector<Vector3D> const &points, double t) const
   {
     // TODO Part 2.
-    return std::vector<Vector3D>();
+    std::vector<Vector3D> output;
+    for (int i = 0; i < points.size() - 1; ++i) {
+      output.push_back((1 - t) * points[i] + t * points[i+1]);
+
+    }
+    return output;
   }
 
   /**
@@ -43,7 +53,11 @@ namespace CGL
   Vector3D BezierPatch::evaluate1D(std::vector<Vector3D> const &points, double t) const
   {
     // TODO Part 2.
-    return Vector3D();
+    std::vector<Vector3D> input = points;
+    while (input.size() != 1) {
+      input = BezierPatch::evaluateStep(input, t);
+    }
+    return input[0];
   }
 
   /**
@@ -53,10 +67,15 @@ namespace CGL
    * @param v         Scalar interpolation parameter (along the other axis)
    * @return Final interpolated vector
    */
-  Vector3D BezierPatch::evaluate(double u, double v) const 
-  {  
+  Vector3D BezierPatch::evaluate(double u, double v) const
+  {
     // TODO Part 2.
-    return Vector3D();
+    std::vector<Vector3D> output;
+    for (int i = 0; i < this->controlPoints.size();++i) {
+      output.push_back(BezierPatch::evaluate1D(this->controlPoints[i], u));
+    }
+
+    return BezierPatch::evaluate1D(output, v);
   }
 
   Vector3D Vertex::normal( void ) const
@@ -65,14 +84,74 @@ namespace CGL
     // Returns an approximate unit normal at this vertex, computed by
     // taking the area-weighted average of the normals of neighboring
     // triangles, then normalizing.
-    return Vector3D();
+
+    Vector3D N( 0., 0., 0. );
+
+    HalfedgeCIter h = this->halfedge();
+    do {
+       FaceCIter face = h->face();
+       N += face->normal();
+
+       HalfedgeCIter h_twin = h->twin();
+       VertexCIter v = h_twin->vertex();
+       h = h_twin->next();
+   } while(h != this->halfedge());
+
+    return N.unit();
   }
 
   EdgeIter HalfedgeMesh::flipEdge( EdgeIter e0 )
   {
     // TODO Part 4.
     // This method should flip the given edge and return an iterator to the flipped edge.
-    return EdgeIter();
+    if (e0->isBoundary()) {
+      return e0;
+    }
+    HalfedgeIter h0 = e0->halfedge();
+    HalfedgeIter h3 = h0->twin();
+
+    HalfedgeIter h1 = h0->next();
+    HalfedgeIter h4 = h3->next();
+
+    HalfedgeIter h2 = h1->next();
+    HalfedgeIter h5 = h4->next();
+
+    FaceIter f0 = h0->face();
+    FaceIter f1 = h3->face();
+
+    VertexIter v0 = h0->vertex();
+    VertexIter v1 = h1->vertex();
+    VertexIter v2 = h5->vertex();
+    VertexIter v3 = h2->vertex();
+
+    EdgeIter e1 = h5->edge();
+    EdgeIter e2 = h4->edge();
+    EdgeIter e3 = h2->edge();
+    EdgeIter e4 = h1->edge();
+
+    v0->halfedge() = h4;
+    v1->halfedge() = h1;
+    v2->halfedge() = h5;
+    v3->halfedge() = h2;
+
+    f0->halfedge() = h0;
+    f1->halfedge() = h3;
+
+    e1->halfedge() = h5;
+    e2->halfedge() = h4;
+    e3->halfedge() = h2;
+    e4->halfedge() = h1;
+
+    h0->setNeighbors(h2, h3, v2, e0, f0);
+    h1->setNeighbors(h3, h1->twin(), v1, e4, f1);
+
+    h2->setNeighbors(h4, h2->twin(), v3, e3, f0);
+    h3->setNeighbors(h5, h0, v3, e0, f1);
+
+    h4->setNeighbors(h0, h4->twin(), v0, e2, f0);
+    h5->setNeighbors(h1, h5->twin(), v2, e1, f1);
+
+    return e0;
   }
 
   VertexIter HalfedgeMesh::splitEdge( EdgeIter e0 )
@@ -94,14 +173,14 @@ namespace CGL
     // 1. Compute new positions for all the vertices in the input mesh, using the Loop subdivision rule,
     // and store them in Vertex::newPosition. At this point, we also want to mark each vertex as being
     // a vertex of the original mesh.
-    
+
     // 2. Compute the updated vertex positions associated with edges, and store it in Edge::newPosition.
-    
+
     // 3. Split every edge in the mesh, in any order. For future reference, we're also going to store some
     // information about which subdivide edges come from splitting an edge in the original mesh, and which edges
     // are new, by setting the flat Edge::isNew. Note that in this loop, we only want to iterate over edges of
     // the original mesh---otherwise, we'll end up splitting edges that we just split (and the loop will never end!)
-    
+
     // 4. Flip any new edge that connects an old and new vertex.
 
     // 5. Copy the new vertex positions into final Vertex::position.
